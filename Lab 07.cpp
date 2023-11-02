d9d3bef88a242b499feab15c86693613cd53f2d2
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-11-01 23:27:30 -0500

    3. Declarando las acciones de la función Torneo
    Implementando las acciones a realizar en la función Torneo.

--- Lab 07.cpp
+++ Lab 07.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 vector<int> Torneo(std::vector<int> habilidades, int N, int K) {
+    int ganador, perdedor;
+
+    for (int juego = 1; juego <= K; juego++) {
+        if (habilidades[0] > habilidades[1]) {
+            ganador = habilidades[0];
+            perdedor = habilidades[1];
+        } else {
+            ganador = habilidades[1];
+            perdedor = habilidades[0];
+        }
+
+        if (juego % (2 * N) == 0) {
+            habilidades.push_back(ganador);
+            habilidades.erase(habilidades.begin(), habilidades.begin() + 2);
+        } else {
+            habilidades.push_back(perdedor);
+            habilidades.erase(habilidades.begin(), habilidades.begin() + 1);
+        }
+    }
+
+    return {perdedor, ganador};
 }
 
 int main()
