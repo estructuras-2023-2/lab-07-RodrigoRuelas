f6c9a37a80226f8a3617f3c913fd01b0f94fc348
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-11-03 07:59:37 -0500

    6. Agregando las 4 entradas presentadas

--- Lab 07.cpp
+++ Lab 07.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 vector<int> Torneo(vector<int> habilidades, int N, int K) {
+    int ganador, perdedor;
+
     for (int juego = 1; juego <= K; juego++) {
-        int ganador, perdedor;
         if (habilidades[0] > habilidades[1]) {
             ganador = habilidades[0];
             perdedor = habilidades[1];
@@ -14,25 +15,51 @@ vector<int> Torneo(vector<int> habilidades, int N, int K) {
         }
 
         if (juego % (2 * N) == 0) {
-            habilidades[0] = ganador;
-            habilidades[1] = perdedor;
+            habilidades.push_back(ganador);
+            habilidades.erase(habilidades.begin(), habilidades.begin() + 2);
         } else {
-            habilidades[1] = perdedor;
+            habilidades.push_back(perdedor);
+            habilidades.erase(habilidades.begin(), habilidades.begin() + 1);
         }
     }
 
-    return habilidades;
+    return {perdedor, ganador};
+}
+
+void imprimirResultado(const vector<int>& resultado, int K) {
+    cout << "Perdedor del juego " << K << ": " << resultado[0] << " Ganador del juego " << K << ": " << resultado[1] << endl;
 }
 
 int main() {
-    vector<int> habilidades = {30, 12};
-    int N = 34;
-    int K = 80;
+    // Entradas
+    vector<int> habilidades1 = {1, 2, 3};
+    int N1 = 2;
+    int K1 = 2;
 
-    vector<int> resultado = Torneo(habilidades, N, K);
+    vector<int> habilidades2 = {49, 24, 26, 12, 5, 33, 25, 30, 35, 41, 46, 23, 21, 3, 38, 43, 11, 19, 34, 29, 20, 32, 39, 7, 50};
+    int N2 = 10;
+    int K2 = 399;
 
-    cout << "Perdedor del juego " << K << ": " << resultado[1] << endl;
-    cout << "Ganador del juego " << K << ": " << resultado[0] << endl;
+    vector<int> habilidades3 = {1, 2, 3};
+    int N3 = 2;
+    int K3 = 4;
+
+    vector<int> habilidades4 = {30, 12};
+    int N4 = 34;
+    int K4 = 80;
+
+    // Resultados
+    vector<int> resultado1 = Torneo(habilidades1, N1, K1);
+    vector<int> resultado2 = Torneo(habilidades2, N2, K2);
+    vector<int> resultado3 = Torneo(habilidades3, N3, K3);
+    vector<int> resultado4 = Torneo(habilidades4, N4, K4);
+
+    // Salidas
+    imprimirResultado(resultado1, K1);
+    imprimirResultado(resultado2, K2);
+    imprimirResultado(resultado3, K3);
+    imprimirResultado(resultado4, K4);
 
     return 0;
 }
+
