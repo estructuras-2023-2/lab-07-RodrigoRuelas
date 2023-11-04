aef752fd65c60a2123577b2ac322dd7776912496
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-11-03 20:50:08 -0500

    Entregable final

---
+++ student_file.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <queue>
+
+vector<int> Torneo(vector<int> H, int N, int K) {
+    queue<int> cola;
+    for (size_t i = 2; i < H.size(); ++i) {
+        cola.push(H[i]);
+    }
+
+    int gameWon = 0;
+    int currentPlayer = H[0];
+    int challengerPlayer = H[1];
+
+    vector<int> result(2);
+    for (int game = 1; game <= K; ++game) {
+        if (currentPlayer > challengerPlayer) {
+            gameWon++;
+        } else {
+            gameWon = 1;
+            swap(currentPlayer, challengerPlayer);
+        }
+        
+        result = {challengerPlayer, currentPlayer};
+        cola.push(challengerPlayer);
+
+        if (gameWon == N) {
+            cola.push(currentPlayer);
+            currentPlayer = cola.front();
+            cola.pop();
+            gameWon = 0;
+        }
+
+        challengerPlayer = cola.front();
+        cola.pop();
+    }
+
+    return result;
+}
+
+void imprimirResultado(const vector<int>& R, int K) {
+    cout << "Perdedor del juego " << K << ": " << R[0] << " Ganador del juego " << K << ": " << R[1] << endl;
+}
